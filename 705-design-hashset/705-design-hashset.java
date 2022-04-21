9912b1216e8c15cf6a618df4954d61b3ee805b27
Harsh-1000 <85452760+Harsh-1000@users.noreply.github.com>
2022-04-21 23:36:56 +0530

    Time: 38 ms (39.76%), Space: 80.9 MB (19.15%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.java
@@ -0,0 +1,35 @@
+class MyHashSet {
+    
+    boolean[] res;
+
+    public MyHashSet() {
+  
+         res = new boolean[1000001];
+    }
+    
+    public void add(int key) {
+        
+        res[key] = true;
+        
+    }
+    
+    public void remove(int key) {
+        
+        res[key] = false;
+        
+    }
+    
+    public boolean contains(int key) {
+        
+        return res[key];
+        
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
