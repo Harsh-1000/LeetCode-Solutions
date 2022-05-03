1b57f15859a88c6d7be355e3b8dd505cdd9305d3
Harsh-1000 <85452760+Harsh-1000@users.noreply.github.com>
2022-05-03 23:41:07 +0530

    Time: 1 ms (58.22%), Space: 43.5 MB (68.59%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -1,17 +1,5 @@
 class Solution {
     
-    private int signCheck(int num)
-    {
-        if(num>0)
-            return 1;
-        else if(num<0)
-            return -1;
-        
-        return 0;
-    }
-    
-    
-    
     
     public int arraySign(int[] nums) {
         
@@ -20,12 +8,12 @@ class Solution {
         
         for(int i=0;i<nums.length;i++)
         {
-            int x=signCheck(nums[i]);
-            
-            if(x==-1)
-                count++;
-            if(x==0)
+        
+            if(nums[i]==0)
                 return 0;
+            
+            if(nums[i]<0)
+                count++;
         }
         
         if(count%2==0)
