d90aa8d8738cec0f508ce42b2675e9349e48be37
Harsh-1000 <85452760+Harsh-1000@users.noreply.github.com>
2022-08-01 11:06:53 +0530

    Time: 1 ms (55.39%), Space: 40.7 MB (65.39%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,22 +1,27 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         
-        int [][] arr = new int[m][n];
+        if(m==1 || n==1)
+            return 1;
         
-        for(int i=0; i<m;i++)  arr[i][0]=1;
+        int[][] path = new int[m][n];
         
+        for(int i=0;i<m;i++)
+            path[i][0]=1;
         
-        for(int i=0;i<n;i++)   arr[0][i]=1;
+        for(int i=0;i<n;i++)
+            path[0][i]=1;
         
         for(int i=1;i<m;i++)
         {
             for(int j=1;j<n;j++)
             {
-                arr[i][j] = arr[i-1][j] + arr[i][j-1];
+                path[i][j]= path[i][j-1] + path[i-1][j];
             }
         }
         
-        return arr[m-1][n-1];
+        return path[m-1][n-1];
+        
         
         
     }
