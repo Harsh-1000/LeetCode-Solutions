0ef089a4d1feff656b56fedb299547e512390612
Harsh-1000 <85452760+Harsh-1000@users.noreply.github.com>
2022-08-16 13:40:24 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (74.07%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -9,19 +9,22 @@
  * }
  */
 class Solution {
-    public ListNode reverseList(ListNode head) {
+    
+    
+    
+    public ListNode reverseList(ListNode head) 
+    {
         
-        if(head==null || head.next==null)
+        if( head==null  || head.next==null )
             return head;
         
-        ListNode newHead = reverseList(head.next);
+        ListNode temp = reverseList(head.next);
         
-        head.next.next = head;
+        head.next.next=head;
+        head.next =null;
         
-        head.next=null;
-        
-        return newHead;
-        
+        return temp;
+    
         
     }
 }
