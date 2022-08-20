e20eb4074a5b284d26827da841df495ef5f8ecde
Harsh-1000 <85452760+Harsh-1000@users.noreply.github.com>
2022-08-20 17:15:07 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (80.31%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,27 +1,18 @@
 class Solution {
-    
-    private static int help(int n, int[] memo)
-    {
+    public int fib(int n) {
         
-        if(n==0) return 0;
-        if(n==1) return 1;
+        int a = 0;
         
-        if(memo[n-1]!=-1)
-            return memo[n-1];
+        int b = 1;
         
-         memo[n-1] =  help(n-1,memo) + help(n-2,memo);
-         return memo[n-1];
-    }
-    
-    public int fib(int n) 
-    {
+        while(n>0)
+        {
+            int c = a+b;
+            a=b;
+            b=c;
+            n--;
+        }
         
-      int[] memo = new int[n];
-        
-      for(int i=0;i<n;i++)
-          memo[i]=-1;
-      
-      return help(n,memo);  
-    
+        return a;
     }
 }
