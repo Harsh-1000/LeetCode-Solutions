f79a74ecc17015f20f61fc83adacc75561747b2d
Harsh-1000 <85452760+Harsh-1000@users.noreply.github.com>
2022-09-08 10:49:47 +0530

    Time: 1 ms (89.88%), Space: 42.5 MB (67.81%) - LeetHub

---
+++ 50-powx-n/50-powx-n.java
@@ -0,0 +1,39 @@
+class Solution {
+    
+
+    public double myPow(double x, int n) {
+      
+     double ans=1.0;   
+     int num =n;   
+     if(n<0)
+     {
+         num=num*-1;
+     }
+     
+     while(num!=0)
+     {
+         if(num%2==0)
+         {
+             x=x*x;
+             num=num/2;
+         }
+         else
+         {
+             ans = ans*x;
+             if(num>0)
+               num=num-1;
+             else
+               num = num+1;  
+         }
+     }
+        
+     if(n<0)
+         return 1/ans;
+     else
+         return ans;
+        
+        
+     
+            
+    }
+}
