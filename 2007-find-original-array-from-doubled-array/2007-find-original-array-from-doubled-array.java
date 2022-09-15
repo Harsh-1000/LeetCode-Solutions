707045044dfa8a6baa1ce4a9606723c108b41dbf
Harsh-1000 <85452760+Harsh-1000@users.noreply.github.com>
2022-09-15 11:32:29 +0530

    Time: 59 ms (87.72%), Space: 60.5 MB (91.62%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,55 +1,35 @@
 class Solution {
     public int[] findOriginalArray(int[] changed) {
         
-        if(changed.length%2!=0)
-             return new int[0];
+        Arrays.sort(changed);
+            
+        if(changed.length%2!=0) return new int[0];
         
-        int max=0;
-        int n = changed.length;
+        int mid = changed.length/2;
         
-        for(int x: changed)
-        {
-            max = Math.max(max,x);
+        int[] res = new int[mid];
+        
+        int[] freq = new int[100001];
+        
+        for(int no : changed)
+            freq[no]++;
+        
+        int idx=0;
+        
+        for(int no: changed){
+            if(freq[no] > 0 && no*2 <= 100000 && freq[no*2]>0){
+                freq[no]--;
+                freq[no*2]--;
+                res[idx++] = no;
+            }
         }
         
-        int[] fre = new int[max+1];
-        
-        for(int i=0;i<n;i++)
-        {
-            fre[changed[i]]++;
+        for(int i=0; i<freq.length; i++){
+            if(freq[i]!=0) return new int[0];
         }
-        
-        if(fre[0]%2!=0)
-            return new int[0];
-        
-        int[] res = new int[n/2];
-        
-        int k=fre[0]/2;
-        
-        
-        for(int i=1;i<max+1;i++)
-        {
-            
-            if(fre[i]==0)
-                 continue;
-            
-            if(i*2 > max || fre[i]>fre[i*2])
-                return new int[0];
-            
-            fre[i*2] -= fre[i];
-            while(fre[i]-- > 0)
-                res[k++] = i;
-        }
-        
-  
-        
-    
         
         return res;
         
-        
-        
-         
-        
+
     }
 }
