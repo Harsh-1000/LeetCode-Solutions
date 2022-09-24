4aea9141c901667855cbc644cd0a53e89fc42009
Harsh-1000 <85452760+Harsh-1000@users.noreply.github.com>
2022-09-24 22:33:38 +0530

    Time: 2 ms (85.41%), Space: 44.7 MB (52.76%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.java
+++ 113-path-sum-ii/113-path-sum-ii.java
@@ -14,37 +14,28 @@
  * }
  */
 class Solution {
-    
-    List<List<Integer>> l1 = new ArrayList<>();
-    
-    private void helper(TreeNode root, int targetSum, Stack st)
-    {
-        if(root==null)
-            return;
-        
-        targetSum -= root.val;
-        st.push(root.val);
-        
-        if(targetSum==0 && root.left==null &&  root.right==null)
-            l1.add(new ArrayList<>(st));
-            
-        helper(root.left,targetSum,st);
-        helper(root.right,targetSum,st);
-         
-        
-        st.pop();
-    }
-    
-    
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        
-        
-        Stack<Integer> st = new Stack();
-        
-        helper(root,targetSum,st);
-        
-        return l1;
-        
-                
+        List<Integer> currentPath = new ArrayList();
+        List<List<Integer>> allPaths = new ArrayList();
+        findPathsRecursive(root,targetSum,currentPath,allPaths);
+        return allPaths;
     }
+    private static void findPathsRecursive(TreeNode currentNode, int sum, List<Integer> currentPath,List<List<Integer>> allPaths){
+         if(currentNode==null)return;
+         // add the current node to the path
+         currentPath.add(currentNode.val);
+         // if the current node is a leaf and its value is equal to sum, save the current path
+         if(currentNode.val==sum && currentNode.left==null && currentNode.right==null)
+             allPaths.add(new ArrayList<Integer>(currentPath));
+         else{
+             // traverse the left sub-tree
+             findPathsRecursive(currentNode.left,sum-currentNode.val,currentPath,allPaths);
+             // traverse the right sub-tree
+             findPathsRecursive(currentNode.right,sum-currentNode.val,currentPath,allPaths);
+         }
+         // remove the current node from the path to backtrack, 
+         // we need to remove the current node while we are going up the recursive call stack.
+         currentPath.remove(currentPath.size()-1);
+      }
+
 }
