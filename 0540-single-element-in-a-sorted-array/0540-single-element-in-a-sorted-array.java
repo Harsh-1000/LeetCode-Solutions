0eb3e30ae16de81a18f3416f5535040cf96a8dd5
Harsh-1000 <85452760+Harsh-1000@users.noreply.github.com>
2022-10-26 11:27:35 +0530

    Time: 0 ms (100.00%), Space: 61 MB (10.60%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,55 +1,27 @@
 class Solution {
-    
-    private int rightMost(int[] nums,int target)
-    {
-        int low = 0;
-        int high = nums.length-1;
-        
-        while(low<=high)
-        {
-            int mid = low + (high-low)/2;
-            
-            if(nums[mid]<=target)
-                low = mid + 1;
-            
-            else
-                high = mid - 1;
-        }
-        
-        return high;
-    }
-    
-    private int leftMost(int[] nums,int target)
-    {
-        int low = 0;
-        int high = nums.length-1;
-        
-        while(low<=high)
-        {
-            int mid = low + (high-low)/2;
-            
-            if(nums[mid]>=target)
-                high = mid - 1;
-            
-            else
-                low = mid + 1;
-        }
-        
-        return low;
-    }
-    
-    
     public int singleNonDuplicate(int[] nums) {
         
-        for(int i=0;i<nums.length;i++)
+        if(nums.length==1)
+            return nums[0];
+        
+        int low = 0;
+        int high = nums.length-1;
+        
+        while(low<high)
         {
-            int count = rightMost(nums,nums[i]) - leftMost(nums,nums[i]);
+            int mid = low + (high-low)/2;
             
-            if(count==0)
-                return nums[i];
+            if(nums[mid]==nums[mid+1])
+                mid = mid -1;
+            
+            if((mid-low+1)%2==0)
+                low=mid+1;
+            
+            else
+                high=mid;
         }
         
-        return -1;
+        return nums[low];
         
     }
 }
